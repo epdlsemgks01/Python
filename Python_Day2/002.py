863ac84eb72c6e8f4d772da690c07a1c68d721ea
epdlsemgks01 <67623914+epdlsemgks01@users.noreply.github.com>
2026-05-14 05:19:34 +0900

    파이썬 둘째날 공부기록

---
+++ Python_Day2/002.py
@@ -0,0 +1,7 @@
+"{}".format(10)
+"{}{}".format(10, 20)
+"{}{}{}{}{}".format(101, 202, 303, 404, 505)
+
+string_a="{}".format(10)
+print(string_a)
+print(type(string_a))
---
+++ Python_Day2/003.py
@@ -0,0 +1,27 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("#하나만 출력합니다")
+#하나만 출력합니다
+>>> print("Hello Python Programming...!")
+Hello Python Programming...!
+>>> print()
+... 
+
+>>> print("#여러 개를 출력합니다")
+#여러 개를 출력합니다
+>>> print(10,20,30,40,50)
+10 20 30 40 50
+>>> print("동해물과", "백두산이", "마르고 닳도록")
+동해물과 백두산이 마르고 닳도록
+>>> print()
+
+>>> print("#아무것도 출력하지 않습니다.")
+#아무것도 출력하지 않습니다.
+>>> print("---확인 전용선---")
+---확인 전용선---
+>>> print()
+
+>>> print()
+
+>>> print("---확인 전용선---")
+---확인 전용선---
